6e24d2915bf062867b370c46b5aa25639d3ca5a6
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-05-18 05:45:25 +0530

    Update start.py

--- lycia/Lycia/start.py
+++ lycia/Lycia/start.py
@@ -3,13 +3,13 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 LYCIA_START = """
-I am Lycia [リュキア], An Intelligent ChatBot
+I am Nezuko-Chan, An Intelligent ChatBot
 """
 
 
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
+                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/DEVILTG/Nezukochanchatbot"), InlineKeyboardButton("Support Group", url = "https://t.me/devils_territory")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
