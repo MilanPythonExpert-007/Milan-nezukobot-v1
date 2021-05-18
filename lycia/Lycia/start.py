4b022bd58fb54754ebb01c80ec31012edb654f5e
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-05-18 05:54:30 +0530

    Update start.py

--- lycia/Lycia/start.py
+++ lycia/Lycia/start.py
@@ -10,6 +10,6 @@ I am Nezuko-Chan, An Intelligent ChatBot
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/DEVILTG/Nezukochanchatbot"), InlineKeyboardButton("Support Group", url = "https://t.me/devils_territory")]
+                [InlineKeyboardButton("Group manager Bot", url = "https://t.me/yumekojabami_robot"), InlineKeyboardButton("Github", url = "https://github.com/DEVILTG/Nezukochanchatbot"), InlineKeyboardButton("Support Group", url = "https://t.me/devils_territory")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
