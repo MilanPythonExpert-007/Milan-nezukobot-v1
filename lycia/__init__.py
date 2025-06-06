451c79d5c1cdbccda387878b2ed143433e0531b3
MilanPythonExpert-007 <chetnapatel9100@gmail.com>
2025-06-06 12:18:41 +0530

    Update __init__.py

--- lycia/__init__.py
+++ lycia/__init__.py
@@ -1,8 +1,8 @@
 import os
 from pyrogram import Client
 
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-TOKEN = os.environ.get("TOKEN", None)
+API_ID = os.environ.get("24082016", None)
+API_HASH = os.environ.get("81af1b2969f06110e0901aa41bfb932d", None)
+TOKEN = os.environ.get("8084842403:AAEUFB_06TBlAhy3J-0daAMQwL7nqHBsLsk", None)
 
 LYCIA = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
